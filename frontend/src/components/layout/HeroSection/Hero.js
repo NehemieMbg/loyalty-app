2d20f70d87fde7ad355b194a76a9e33e3ab1fa0d
NehemieMbg <nehemie.mbg@gmail.com>
2023-05-03 03:36:03 +0200

    Hero Heading Size

--- frontend/src/components/layout/HeroSection/Hero.js
+++ frontend/src/components/layout/HeroSection/Hero.js
@@ -6,11 +6,7 @@ const Hero = () => {
   return (
     <div className="hero-section">
       <div className="hero-text__container">
-        <h1 className="hero-heading item-1">
-          Rent Your
-          <br />
-          Dream Sport Car
-        </h1>
+        <h1 className="hero-heading item-1">Rent Your Dream Sport Car</h1>
         <p className="hero-text item-2">
           We deliver across Lille. Unlimited mileage, additional drivers and
           insurance.
